e7bb9e279da4ff22c2bae74083c7943d588f749a
WumaCoder <15804854160@163.com>
2022-06-13 11:51:54 +0800

    chore: 添加勾子方法

--- packages/shared/src/injectHook.ts
+++ packages/shared/src/injectHook.ts
@@ -18,9 +18,32 @@ export function injectHookBefore<T>(
     configurable: true,
   };
   Object.defineProperty(instance, name, {
-    value: function () {
+    value: function() {
       methodFn(this, ...arguments);
       return _method && _method.apply(this, arguments);
+    },
+    enumerable: desc.enumerable,
+    writable: desc.writable,
+    configurable: desc.configurable,
+  });
+}
+
+export function injectHookAfter<T>(
+  instance: T,
+  name: FunctionKeyFilter<T>,
+  methodFn: MethodFn<T>
+) {
+  const _method = instance[name] as any;
+  const desc = Object.getOwnPropertyDescriptor(instance, name) || {
+    enumerable: true,
+    writable: true,
+    configurable: true,
+  };
+  Object.defineProperty(instance, name, {
+    value: function() {
+      const res = _method && _method.apply(this, arguments);
+      methodFn(this, ...arguments);
+      return res;
     },
     enumerable: desc.enumerable,
     writable: desc.writable,
