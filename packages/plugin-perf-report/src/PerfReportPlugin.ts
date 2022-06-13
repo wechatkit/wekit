02feb59d0a9a7c257439a9b86b2d21df98e0245f
WumaCoder <15804854160@163.com>
2022-06-13 16:18:38 +0800

    feat: 上报功能开发完成

--- packages/plugin-perf-report/src/PerfReportPlugin.ts
+++ packages/plugin-perf-report/src/PerfReportPlugin.ts
@@ -1,11 +1,66 @@
 import { Plugin, Wekit } from "@wekit/core";
-import { injectHookBefore } from "@wekit/shared";
+
+const LOG_PREFIX = "[perf-report]:";
+type PerfReportPluginOptions = {
+  onReport?: (pagePerfReport: AnyObject) => void; // 上报页面性能数据
+};
 
 export class PerfReportPlugin implements Plugin {
+  private pagePerfReport: AnyObject = {};
+  private reportHandler = () => {
+    console.table(this.pagePerfReport);
+    if (this.options.onReport) {
+      this.options.onReport(this.pagePerfReport);
+    }
+  };
+
+  constructor(private options: PerfReportPluginOptions = {}) {}
+
   install(ctx: Wekit) {
-    console.log(ctx);
+    const updatePagePerfReport = (page: any, isForce?: boolean | undefined) => {
+      const route = page.route;
+      const now = Date.now();
+      const cost = now - this.pagePerfReport[route].start;
+      const dur = now - this.pagePerfReport[route].last;
+      if (isForce || dur < 100) {
+        this.pagePerfReport[route].cost = cost;
+        this.pagePerfReport[route].last = now;
+      }
+    };
+
+    ctx.pageEventEmitter.on("onPreload", (page: { route: string | number }) => {
+      const start = Date.now();
+      this.pagePerfReport[page.route] = {
+        start,
+        last: start,
+      };
+    });
+
+    ctx.pageEventEmitter.on("onReady", (page: { route: string | number }) => {
+      updatePagePerfReport(page, true);
+      console.log(
+        LOG_PREFIX,
+        `Page ${page.route} Ready time: ${
+          this.pagePerfReport[page.route].cost
+        }ms`
+      );
+    });
+
+    ctx.pageEventEmitter.on("flushView", ({ wk }: any) => {
+      const page = wk.meta.options;
+      updatePagePerfReport(page);
+      console.log(
+        LOG_PREFIX,
+        `Page ${page.route} flushView time: ${
+          this.pagePerfReport[page.route].cost
+        }ms`
+      );
+    });
+
+    wx.onAppHide(this.reportHandler);
   }
+
   uninstall() {
-    throw new Error("Method not implemented.");
+    wx.offAppHide(this.reportHandler);
   }
 }
