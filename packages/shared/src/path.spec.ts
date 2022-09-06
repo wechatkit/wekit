26a11ec64de545419a925790dae789dac69d19ca
WumaCoder <15804854160@163.com>
2022-09-06 12:05:57 +0800

    test(shared): 添加测试文件

---
+++ packages/shared/src/path.spec.ts
@@ -0,0 +1,16 @@
+import { calcRelativePath } from "./path";
+
+describe('path.ts', () => {
+  it('calcRelativePath', () => {
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index')).toBe('/pages/index/index');
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index.js')).toBe('/pages/index/index.js');
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index.json')).toBe('/pages/index/index.json');
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index.wxml')).toBe('/pages/index/index.wxml');
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index.wxss')).toBe('/pages/index/index.wxss');
+    expect(calcRelativePath('/pages/index/index', '/pages/index/index.wxs')).toBe('/pages/index/index.wxs');
+    expect(calcRelativePath('/pages/teacher/teacher', '../index/index.wxs')).toBe('/pages/index/index.wxs');
+    expect(calcRelativePath('', '../index/index.wxs')).toBe('index/index.wxs');
+    expect(calcRelativePath('', '/index/index.wxs')).toBe('/index/index.wxs');
+    expect(calcRelativePath('/pages/teacher/index', '')).toBe('/pages/teacher/index');
+  });
+});
