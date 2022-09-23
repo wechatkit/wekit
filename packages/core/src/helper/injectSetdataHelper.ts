152a2647a77d93a5275aba0e91921e0de71ff3e8
WumaCoder <15804854160@163.com>
2022-09-23 11:42:24 +0800

    fix(core): 优化setData cb调用时机，setData支持Promise

--- packages/core/src/helper/injectSetdataHelper.ts
+++ packages/core/src/helper/injectSetdataHelper.ts
@@ -7,14 +7,17 @@ export function injectSetDataHelper(ctx: any) {
   const wekit = Wekit.globalWekit;
   const wk: Wk = Wk.get(ctx) as Wk;
   function _setData(this: any, data: AnyObject, cb: () => void) {
-    wekit.pageEventEmitter.emit("setData", this, data);
-    for (const key in data) {
-      const value = data[key];
-      setTargetValue(this.data, key, value);
-      wk.updateData[key] = value;
-    }
-    if (cb && typeof cb === "function") wk.updateCallbacks.push(cb);
-    triggerFlush(wk);
+    return new Promise((resolve) => {
+      wekit.pageEventEmitter.emit("setData", this, data);
+      for (const key in data) {
+        const value = data[key];
+        setTargetValue(this.data, key, value);
+        wk.updateData[key] = value;
+      }
+      wk.updateCallbacks.push(resolve);
+      if (cb && typeof cb === "function") wk.updateCallbacks.push(cb);
+      triggerFlush(wk);
+    });
   }
   Object.defineProperty(ctx, "setData", {
     value: _setData,
@@ -49,8 +52,9 @@ function triggerFlush(wk: Wk) {
         updateData,
         Date.now() - updateTime
       );
-      wk.updateCallbacks.forEach((cb) => cb());
+      const cbs = wk.updateCallbacks;
       wk.updateCallbacks = [];
+      cbs.forEach((cb) => cb());
     });
     wk.lock = false;
   }
